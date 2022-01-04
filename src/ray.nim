a3f7a812eb2d0daf999b6a5499cd0140e1a0187b
Seiichi Ariga <seiichi.ariga@gmail.com>
2022-01-04 11:28:49 +0900

    Sphere

--- src/ray.nim
+++ src/ray.nim
@@ -2,8 +2,23 @@ import util
 
 # 光のtype
 type Ray* = object
-  origin*:    Vec3
+  origin*: Vec3
   direction*: Vec3
 
 func pointAtParameter*(r: Ray, t: float): Vec3 =
   r.origin + r.direction * t
+
+func hitSphere*(ray: Ray, center: Vec3, radius: float): bool =
+  let oc = ray.origin - center
+  let a = dot(ray.direction, ray.direction)
+  let b = 2.0 * dot(oc, ray.direction)
+  let c = dot(oc, oc) - radius * radius
+  let discriminant = b * b - 4.0 * a * c
+  discriminant > 0.0
+
+func color*(ray: Ray): Vec3 =
+  if(ray.hitSphere(Vec3(x: 0, y: 0, z: -1), 0.5)):
+    return Vec3(x: 1, y: 0, z: 0)
+  let unitDirection = normalize(ray.direction)
+  let t = 0.5*(unitDirection.y + 1.0)
+  return (1.0-t)*Vec3(x: 1.0, y: 1.0, z: 1.0)+t*Vec3(x: 0.5, y: 0.7, z: 1.0)
--- src/vec3.nim
+++ src/vec3.nim
@@ -1,5 +1,6 @@
 import std/streams
 import util
+import ray
 
 const nx = 200
 const ny = 100
@@ -10,10 +11,19 @@ if not isNil(strm):
   strm.writeLine("P3")
   strm.writeLine(nx, " ", ny)
   strm.writeLine("255")
+
+  let lowerLeftCorner = Vec3(x: -2.0, y: -1.0, z: -1.0)
+  let horizontal = Vec3(x: 4.0, y: 0.0, z: 0.0)
+  let vertical = Vec3(x: 0.0, y: 2.0, z: 0.0)
+  let origin = Vec3(x: 0.0, y: 0.0, z: 0.0)
+
   for j in 1..ny:
     for i in 1..nx:
-      let color = Vec3(x: i/nx, y: j/ny, z: 0.5)
-      let ir = int(color.r*255)
-      let ig = int(color.g*255)
-      let ib = int(color.b*255)
+      let u = float(i) / float(nx)
+      let v = float(j) / float(ny)
+      let r = Ray(origin: origin, direction: lowerLeftCorner + u * horizontal + v * vertical)
+      let col = color(r)
+      let ir = int(col.r*255.99)
+      let ig = int(col.g*255.99)
+      let ib = int(col.b*255.99)
       strm.writeLine(ir, " ", ig, " ", ib)
