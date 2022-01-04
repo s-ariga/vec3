fe91df6eb3b77f59f61762ced8369a2d4b4ff373
Seiichi Ariga <seiichi.ariga@gmail.com>
2022-01-04 11:38:15 +0900

    Surface Normal

--- src/ray.nim
+++ src/ray.nim
@@ -1,3 +1,5 @@
+import math
+
 import util
 
 # 光のtype
@@ -8,17 +10,22 @@ type Ray* = object
 func pointAtParameter*(r: Ray, t: float): Vec3 =
   r.origin + r.direction * t
 
-func hitSphere*(ray: Ray, center: Vec3, radius: float): bool =
+func hitSphere*(ray: Ray, center: Vec3, radius: float): float =
   let oc = ray.origin - center
   let a = dot(ray.direction, ray.direction)
   let b = 2.0 * dot(oc, ray.direction)
   let c = dot(oc, oc) - radius * radius
   let discriminant = b * b - 4.0 * a * c
-  discriminant > 0.0
+  if (discriminant < 0):
+    return -1.0
+  else:
+    return (-b - sqrt(discriminant)) / (2.0 * a)
 
 func color*(ray: Ray): Vec3 =
-  if(ray.hitSphere(Vec3(x: 0, y: 0, z: -1), 0.5)):
-    return Vec3(x: 1, y: 0, z: 0)
+  let t1 = ray.hitSphere(Vec3(x: 0, y: 0, z: -1), -0.5)
+  if(t1 > 0.0):
+    let N: Vec3 = normalize(ray.pointAtParameter(t1) - Vec3(x: 0, y: 0, z: -1))
+    return 0.5 * Vec3(x: N.x + 1.0, y: N.y + 1.0, z: N.z + 1.0)
   let unitDirection = normalize(ray.direction)
-  let t = 0.5*(unitDirection.y + 1.0)
-  return (1.0-t)*Vec3(x: 1.0, y: 1.0, z: 1.0)+t*Vec3(x: 0.5, y: 0.7, z: 1.0)
+  let t2 = 0.5*(unitDirection.y + 1.0)
+  return (1.0 - t2) * Vec3(x: 1.0, y: 1.0, z: 1.0) + t2 * Vec3(x: 0.5, y: 0.7, z: 1.0)
